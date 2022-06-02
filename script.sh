3730e88492210f409eac5b6bde0838185b1f6b02
Monse <monserrat.gonzalez95@gmail.com>
2022-06-01 22:14:26 -0500

    integración de zenity

--- script.sh
+++ script.sh
@@ -14,25 +14,28 @@ fi
 cat /lib/systemd/system/"$servicio".service 1>> stout.log 2>> stderr.log
 
 if [ $? == 0 ]; then
-    echo "${verde} El servicio "$servicio" está instalado correctamente"
+    echo "El servicio "$servicio" está instalado correctamente"
     punt1=10
 else
-    echo "${rojo} El servicio "$servicio" no está instalado"
+    echo " El servicio "$servicio" no está instalado"
     punt1=0
 fi
 return $punt1
 }
 
 directorio(){
-    echo "${amarillo} Ingresa tu nombre, recuerda que tu nombre deberá estar configurado el virtual host en apache"
-    read nombre       
-    apache2ctl -S 2> stderr.log| grep -o -E 'alias (.*)|(namevhost|server) (.*)\s' | cut -d ' ' -f 2 | grep "$nombre" 
+       
+    apache2ctl -S | grep -o -E 'alias (.*)|(namevhost|server) (.*)\s' | cut -d ' ' -f 2 | grep $NOMBRE
     if [ $? == 0 ]; then
-    echo "${verde} El servicio "$servicio" esta configurado en el virtual host mostrado anteriormente" 
+    a2query -s| grep $NOMBRE  | xargs -L1 -I %  zenity --width=250 --height=250 --info --text=% 
+
+    echo "$El servicio "$servicio" esta configurado en el virtual host mostrado anteriormente" 
     punt4=10
     else
-    echo "${rojo} No existe el virtual host con tu nombre, a continuación verás los virtual host que configuraste : "
-    apache2ctl -S 2>stderr.log| grep -o -E 'alias (.*)|(namevhost|server) (.*)\s' | cut -d ' ' -f 2
+    zenity --error --text "No existe el virtual host con tu nombre, a continuación verás los virtual host que configuraste :"
+    a2query -s | xargs -L1 -I %  zenity --width=250 --height=250 --info --text=% 
+
+
 
     
     fi
@@ -71,4 +74,39 @@ puntf=$((punt1+punt2+punt3+punt4))
 echo "${blanco}Tu puntuacion es:"  $puntf
 }
 
-evaluacion $1 $2
+menu(){
+    NOMBRE=$(zenity --entry --title="Nombre de Alumno" --text="Introduce tu nombre")
+    zenity --info --title="Evaluación automática" --width=250  --text="Se mostrará un menú para seleccionar el servicio a evaluar."
+    OPCION="0"
+    until [ $OPCION == "3" ]
+    do
+    OPCION=$(zenity --list --title="Seleccione la opción a ejecutar" --column="Id" --column="Opcion" 1 "Evaluar servicio APACHE" 2 "Evaluar servicio DNS" 3 "Salir")
+    case $OPCION in
+
+    1) servicio='apache'
+       puerto='80'
+       instalacion $servicio $puerto | xargs -L1 -I %  zenity --width=250 --height=250 --info --text=% 
+        
+       directorio $NOMBRE $servicio |  xargs -L1 -I %  zenity --width=250 --height=250 --info --text=% 
+
+
+       ;;
+
+    2) servicio='dns'
+       puerto='53'
+       instalacion $servicio $puerto | xargs -L1 -I %  zenity --width=250 --height=250 --info --text=%
+       ;;
+    esac
+    done
+     
+
+    
+
+    
+
+
+
+
+}
+menu
+#evaluacion $1 $2
