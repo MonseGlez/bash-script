117b81a51d5ecf022a0b93cb65358ade06a04e37
Monse <monserrat.gonzalez95@gmail.com>
2022-11-05 09:22:38 -0600

    ok

--- leerxls.py
+++ leerxls.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
-from threading import main_thread
-from unittest import result
 import pandas as pd
-import numpy as np
-import re
+import xlsxwriter
+
+
+
 file_loc = "listaAsistencia.xls"
-
-matriculas = pd.read_excel(file_loc, usecols= 'D',skiprows= lambda x: x<12)
-nombres = pd.read_excel(file_loc, usecols= 'H',skiprows= lambda x: x<12)
-
-print(matriculas.dropna())
+matriculas = pd.read_excel(file_loc, usecols= 'D',skiprows= lambda x: x<11)
+nombres = pd.read_excel(file_loc, usecols= 'H',skiprows= lambda x: x<11)
+print(tuple(x.strip('()').split(',') for x in matriculas.dropna() ))
 print(nombres.dropna())
+matricula_data=matriculas.dropna().values.tolist()    
+df = pd.DataFrame({'Matricula': matricula_data,'Nombre Alumno':nombres.dropna().values.tolist(),'DNS':'','Apache':''}).to_excel('listaEvaluacion.xlsx',index=False)
 
--- script.sh
+++ script.sh
@@ -1,90 +1,95 @@
 #!/bin/bash
+rojo=`tput setaf 1`
+amarillo=`tput setaf 3`
+verde=`tput setaf 2`
+blanco=`tput setaf 7`
+
 
 instalacion(){
     if [ "$1" == "apache" ]; then
-        servicio="apache2"
+    servicio="apache2"
 
     elif [ "$1" == "dns" ];then
-        servicio="bind9"
+    servicio="bind9"
     fi
-    cat /lib/systemd/system/"$servicio".service 1>> stout.log 2>> stderr.log
 
+    cat /lib/systemd/system/"$servicio".service 1>> $file_respuesta 2>> $file_errores
     if [ $? == 0 ]; then
-        echo "El servicio "$servicio" está instalado correctamente"
-    else
-        zenity --error --text "El servicio $servicio no está instalado"
+        echo "${verde} El servicio "$servicio" está instalado correctamente"
+        echo " El servicio "$servicio" está instalado correctamente" >> $file_evaluacion
 
+        punt1=10
+    else
+        echo "${rojo} El servicio "$servicio" no está instalado"
+        echo " El servicio "$servicio" no está instalado" >> $file_evaluacion
+
+        punt1=0
     fi
+    return $punt1
 }
 
-directorio(){
-       
-    a2query -s| grep $MATRICULA
-    if [ $? == 0 ]; then
-    a2query -s| grep $MATRICULA  | xargs -L1 -I %  zenity --info --text=% 
-    zenity  --info --text="$El servicio "$servicio" esta configurado en el virtual host mostrado anteriormente" 
 
-    else
-    zenity --error --text "No existe el virtual host con tu MATRICULA, a continuación verás los virtual host que configuraste :"
-    a2query -s | xargs -L1 -I %  zenity  --info --text=% 
-
-
-
-    
-    fi
-             
-}
 
 puerto(){
 
-escucha=$(netstat -plnt 2>> stderr.log | grep $2 | awk {'print $6'})
+escucha=$(netstat -plnt 2>> $file_errores | grep $2 | awk {'print $6'})
 
 if [ "$escucha" = "LISTEN" ]; then 
-    echo "${verde} El servicio $1 corriendo en el puerto $2"
+    echo "${verde} El servicio $1 corriendo en el puerto $2" 
+    echo "El servicio $1 corriendo en el puerto $2" >> $file_evaluacion
+
     punt2=10
 else
-    zenity --error --text "El servicio $1 no está en el puerto $2"
+    echo "${rojo} El servicio $1 no está ejecutandose en el puerto $2"
+    echo "El servicio $1 no está ejecutandose en el puerto $2" >> $file_evaluacion
 
     punt2=0
 fi
+return $punt2
 }
 
 
+evaluacion(){
 
+punt1=0
+punt2=0
+punt3=0
+punt4=0
 
-menu(){
-    MATRICULA=$(zenity --entry --title="Ingresa tu maticula de Alumno" --text="Introduce tu MATRICULA")
-    
-    zenity --info --title="Evaluación automática"  --text="Se mostrará un menú para seleccionar el servicio a evaluar."
-    OPCION="0"
-    
-    until [ $OPCION == "3" ]
-    do
-    OPCION=$(zenity --list --title="Seleccione la opción a ejecutar" --column="Id" --column="Opcion" 1 "Evaluar servicio APACHE" 2 "Evaluar servicio DNS" 3 "Salir")
-    case $OPCION in
-
-    1) servicio='apache'
-       
-       puerto='80'
-       instalacion $servicio $puerto | xargs -L1 -I %  zenity  --info --text=% 
-       puerto $servicio $puerto |  xargs -L1 -I %  zenity --info --text=% 
-    
-       directorio $MATRICULA $servicio |  xargs -L1 -I %  zenity --width=250 --height=250 --info --text=% 
-       echo "$servicio,$puerto, $MATRICULA, $servicio">> test.csv
-
-
-       ;;
-
-    2) servicio='dns'
-       puerto='53'
-       instalacion $servicio $puerto | xargs -L1 -I %  zenity --width=250 --height=250 --info --text=%
-       puerto $servicio $puerto |  xargs -L1 -I %  zenity --width=250 --height=250 --info --text=% 
-       ;;
-    esac
-    done
-
-
+puntf=0
+instalacion $1 
+puerto $1 $2
+puntf=$((punt1+punt2+punt3+punt4))
+echo "${blanco}Tu puntuacion es:"  $puntf
+echo "Tu puntuacion es:"  $puntf "Final de evaluacion"  >> $file_evaluacion
 
 }
-menu
-#evaluacion $1 $2
+modo_uso(){
+echo "El modo de uso esperado es:  ./script nombre_servicio puertoesperado  por ejemplo: ./script apache 80"
+}
+modo_uso
+echo "ingresa tu matricula"
+read matricula
+file_evaluacion=/home/monse/evaluacion/retroalimentacion-$matricula-$(date +"%d-%m-%Y").txt
+file_errores=/home/monse/evaluacion/salidaerror-$matricula-$(date +"%d-%m-%Y").txt
+file_respuesta=/home/monse/evaluacion/salida-$matricula-$(date +"%d-%m-%Y").txt
+
+
+if [ -f $file_evaluacion ]
+then
+   echo "Ya está creado el fichero, saltando creacion" 
+   date
+else
+   echo "El fichero no existe, creando"
+
+   touch $file_evaluacion
+
+fi
+
+
+
+evaluacion $1 $2
+
+
+
+
